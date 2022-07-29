beae64c5443d59a4c7c4a96e747b8b382d88b3de
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-29 03:13:32 -0500

    working through tests and parent class properties and methods

--- __Tests__/Employee.test.js
+++ __Tests__/Employee.test.js
@@ -1,12 +1,28 @@
 const Employee = require('../lib/Employee');
 jest.mock('../lib/Employee');
 
-console.log(new Employee());
+// console.log(new Employee());
 
 test('creates an employee object', () => {
     const employee = new Employee('Ryan');
 
     expect(employee.name).toBe('Ryan');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe(`${employee.name}@email.com`);
+});
+
+test('get employee name', () => {
+    const employee = new Employee('Ryan');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test('get employee email', () => {
+    const employee = new Employee('Ryan');
+
+    expect(employee.getEmail()).toBe(`${employee.name}@email.com`);
+});
+
+test('get employee role', () => {
+    const employee = new Employee('Ryan');
+
+    expect(employee.getRole()).toBe('Employee');
 });
