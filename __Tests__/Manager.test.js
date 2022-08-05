a7f489853d344dd059b3be0847057794538c4387
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-04 20:33:19 -0500

    fixed an error in the manager test

--- __Tests__/Manager.test.js
+++ __Tests__/Manager.test.js
@@ -4,13 +4,13 @@ jest.mock('../lib/Manager');
 console.log(new Manager());
 
 test('creates a manager object', () => {
-    const manager = new Manager('Patti');
+    const manager = new Manager('Ryan');
 
-    expect(manager.name).toBe('Patti');
+    expect(manager.name).toBe('Ryan');
 });
 
 test('creates office number', () => {
-    const manager = new Manager('Patti');
+    const manager = new Manager('Ryan');
 
     expect(manager.getOfficeNumber()).toBe(manager.office);
 });
